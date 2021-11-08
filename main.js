e514a5c18d9a1806d0cdab95baae97e9422964a7
Alejandro Hernández <j.ahr@hotmail.com>
2021-11-08 17:30:04 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const fpost = ({ span = 1, img, name, tags, pageURL }) => {
+    let arr = tags.split(','); 
+    return (
+        ` 
+        <div class="card two span-${span} span-sm-1">
+            <div class="two__img_1">
+                <a href="${pageURL}" target="_blank">
+                    <img class="border-radius-1 cursor" src="${img}" alt="${name}">
+                </a>
+            </div>
+        </div>
+        `
+    )
+}
+
+const getData = () => {
+
+    fetch('https://pixabay.com/api?key=20970363-e25f75efc680fcf7698affe51&per_page=200')
+    .then(response => response.json())
+    .then((data) => {
+
+        let posts = '';
+        data.hits.forEach((element, index) => {
+            index+=1
+            posts += fpost({ img:element.largeImageURL, name:"image1", tags:element.tags, pageURL:element.pageURL })
+        });
+        document.getElementById('posts').innerHTML = posts
+
+    })
+}
+getData();
